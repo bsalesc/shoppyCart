a030d9f999e7dcd458771375e5862ef9852edbcc
bsalesc <bsales.c@gmail.com>
2018-08-22 23:47:26 +0200

    Fixing problem with depedency injection.

--- client/src/app/features/items/edit/edit.module.ts
+++ client/src/app/features/items/edit/edit.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditComponent } from './edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ItemService } from '../../../services/item.service';
 
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   declarations: [EditComponent],
+  providers: [ItemService],
   exports: [EditComponent],
 })
 export class EditModule {}
