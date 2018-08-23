61bc1bcd3988bee9ef238b59895b3786170d6ea6
bsalesc <bsales.c@gmail.com>
2018-08-23 20:53:36 +0200

    Add more tests.

---
+++ server/src/controllers/item.controller.spec.ts
@@ -0,0 +1,103 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import 'mocha';
+import { ItemController } from './item.controller';
+import { mockRes, mockReq } from 'sinon-express-mock';
+import { Item } from '../models/item.model';
+import { IItem } from '../interfaces/item.interface';
+
+describe('Item controller', () => {
+  const controller = new ItemController();
+  const req = mockReq();
+  const res = mockRes();
+
+  const item: IItem = {
+    bought: false,
+    description: 'description',
+    id: 'id',
+    price: 1,
+    quantity: 1,
+  };
+
+  let sandbox: sinon.SinonSandbox;
+  beforeEach(() => (sandbox = sinon.createSandbox()));
+  afterEach(() => sandbox.restore());
+
+  describe('create function', () => {
+    it('should create an item', async () => {
+      sandbox.stub(Item, 'create').resolves(item);
+      await controller.create(req, res);
+
+      expect(res.json.lastCall.args[0]).to.deep.equal({
+        success: true,
+        data: item,
+      });
+    });
+
+    it('should throw an error', async () => {
+      sandbox.stub(Item, 'create').throws();
+      await controller.create(req, res);
+
+      expect(res.json.lastCall.args[0]).to.deep.property('error');
+    });
+  });
+
+  describe('delete function', () => {
+    it('should delete an item', async () => {
+      sandbox.stub(Item, 'deleteOne').resolves();
+      await controller.delete(req, res);
+
+      expect(res.json.lastCall.args[0]).to.deep.equal({
+        success: true,
+      });
+    });
+
+    it('should throw an error', async () => {
+      sandbox.stub(Item, 'deleteOne').throws();
+      await controller.delete(req, res);
+
+      expect(res.json.lastCall.args[0]).to.deep.property('error');
+    });
+  });
+
+  describe('get function', () => {
+    it('should return items', async () => {
+      sandbox.stub(Item, 'find').resolves([item]);
+      await controller.get(req, res);
+
+      expect(res.json.lastCall.args[0]).to.deep.equal({
+        success: true,
+        data: [item],
+      });
+    });
+
+    it('should throw an error', async () => {
+      sandbox.stub(Item, 'find').throws();
+      await controller.get(req, res);
+
+      expect(res.json.lastCall.args[0]).to.deep.property('error');
+    });
+  });
+
+  describe('update function', () => {
+    it('should update an item', async () => {
+      sandbox.stub(Item, 'findOne').resolves(item);
+      sandbox.stub(Item, 'updateOne').resolves();
+
+      await controller.update(req, res);
+
+      expect(res.json.lastCall.args[0]).to.deep.equal({
+        success: true,
+        data: item,
+      });
+    });
+
+    it('should throw an error', async () => {
+      sandbox.stub(Item, 'findOne').throws();
+      sandbox.stub(Item, 'updateOne').throws();
+      await controller.update(req, res);
+
+      expect(res.json.lastCall.args[0]).to.deep.property('error');
+    });
+  });
+});
