282522396d472294953a48b0901841876d18a689
bsalesc <bsales.c@gmail.com>
2018-09-18 08:39:35 +0200

    Add image files

--- client/src/environments/environment.app.prod.ts
+++ client/src/environments/environment.app.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'http://easyshopping.eastus.cloudapp.azure.com/',
+  api_url: 'http://easyshopping.eastus.cloudapp.azure.com/api/',
 };
--- mobile/deploy.sh
+++ mobile/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ../client/
-yarn deploy:app --configuration=app:local
+yarn deploy:app --configuration=app:production
