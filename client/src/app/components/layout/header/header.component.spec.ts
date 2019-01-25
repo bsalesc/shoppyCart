6d8088bbd398f0bc4a9635ba21cfe51775ee7603
bsalesc <bsales.c@gmail.com>
2019-01-25 11:51:46 +0100

    Fix client unit tests

--- client/src/app/components/layout/header/header.component.spec.ts
+++ client/src/app/components/layout/header/header.component.spec.ts
@@ -5,6 +5,8 @@ import { SharedModule } from '../../shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import { AppModule } from 'src/app/app.module';
+import { SideNavComponent } from '../side-nav/side-nav.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -13,7 +15,7 @@ describe('HeaderComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SharedModule, HttpClientModule, RouterModule.forRoot([])],
-      declarations: [HeaderComponent],
+      declarations: [HeaderComponent, SideNavComponent],
       providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
     }).compileComponents();
   }));
