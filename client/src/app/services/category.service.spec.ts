7e72976f42b650191441abf5ac501c1e45fd2994
bsalesc <bsales.c@gmail.com>
2019-02-04 08:59:47 +0100

    Fix category service spec to pass the build

--- client/src/app/services/category.service.spec.ts
+++ client/src/app/services/category.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CategoryService } from './category.service';
+import { HttpService } from '../utils';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('CategoryService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CategoryService]
-    });
+    TestBed.configureTestingModule({ providers: [CategoryService], imports: [HttpClientModule] });
   });
 
   it('should be created', inject([CategoryService], (service: CategoryService) => {
